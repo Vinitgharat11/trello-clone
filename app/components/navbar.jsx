adb9748b386b2fc9f202dda630edeadd12b5e6ae
vint <vinitgharat99@gmail.com>
2023-09-22 14:58:02 +0530

    home page done(mobile version)

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,15 +1,21 @@
 import Image from 'next/image'
 import {CgMenuGridO} from 'react-icons/cg'
 import {BsArrowDownShort} from 'react-icons/bs'
+import {HiOutlinePlus,HiOutlineSearch} from 'react-icons/hi'
 import logo from '../assets/icon.png'
 export default function Navbar() {
   return (
     <nav>
-      <div className="Nav-container flex items-center">
-      <CgMenuGridO/>
-      <Image src={logo} alt='lofo' width={50} height={50} className='grayscale contrast-200'/>
-        <span className='flex'>More <BsArrowDownShort/></span>
+      <div className="Nav-container flex items-center justify-between p-3 border-b-2">
+      <CgMenuGridO size={25} className='text-slate-500'/>
+      <Image src={logo} alt='lofo' width={80} height={50} className='grayscale contrast-200'/>
+        <span className='flex text-lg font-bold items-center mx-2 gap-2 text-slate-500'>More <BsArrowDownShort size={20}/></span>
+        <div className="bg-blue-500 p-1 rounded text-xl">
+        <HiOutlinePlus className='text-white'/>
+        </div>
+        <HiOutlineSearch className='text-xl text-slate-500'/>
       </div>
+      
     </nav>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,59 @@
 import Navbar from "./components/navbar";
+import { FaTrello } from "react-icons/fa";
+import { BsPerson, BsGrid } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
 
+const Tabs = [
+  {
+    icon: <FaTrello />,
+    name: "Boards",
+  },
+  {
+    icon: <BsGrid />,
+    name: "views",
+  },
+  {
+    icon: <BsPerson />,
+    name: "Members",
+  },
+  {
+    icon: <FiSettings />,
+    name: "Settings",
+  },
+];
 export default function Home() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <div className="title">
+        <p className="uppercase font-bold text-slate-500 pt-10 mx-3 py-2">
+          your workspaces
+        </p>
+      </div>
+      <div className="workspaces px-3 flex items-center">
+        <div className="workspaces-logo bg-green-600 text-white p-1 w-9 text-center rounded-md">
+          <p className="text-lg font-bold">T</p>
+        </div>
+        <p className="mx-3 font-bold">Trello Workspace</p>
+      </div>
+      <div className="grid grid-cols-2 m-3">
+        {Tabs.map((i) => (
+          <div className="bg-slate-100 flex items-center m-1 py-2 gap-1 text-[15px]  px-3 rounded-md">
+            {i.icon}
+            <p className="text-sm font-bold ">{i.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="bords">
+        <div className="bg-slate-200 p-10 mx-10 rounded-sm text-center">
+          <p className="font-light">Create new Board</p>
+        </div>
+      </div>
+      <div className="mx-3 my-5">
+        <div className="bg-slate-100 m-1 py-2 text-[15px] px-3 rounded-md w-9/12">
+          <p className="text-sm font-bold ">view all closed boards</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
